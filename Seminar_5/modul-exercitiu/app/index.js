adf4de616570cdd92d78e5bfde73e3836134626b
anamaria1203 <anamaria.vasile2005112@gmail.com>
2025-11-03 14:19:44 +0000

    Seminar_5

---
+++ Seminar_5/modul-exercitiu/app/index.js
@@ -0,0 +1,4 @@
+import { PI, circleArea } from "../lib/mathModule.js";
+
+console.log("PI =", PI);
+console.log("Aria cercului cu raza 2 =", circleArea(2));
---
+++ Seminar_5/modul-exercitiu/lib/mathModule.js
@@ -0,0 +1,2 @@
+export const PI = 3.1415926;
+export const circleArea = (r) => PI * r * r;
