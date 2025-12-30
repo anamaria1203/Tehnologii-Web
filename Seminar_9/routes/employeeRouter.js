314b56e58e0840db2479155d3c57dc5aa0e942db
anamaria1203 <anamaria.vasile2005112@gmail.com>
2025-12-30 16:45:01 +0000

    Seminar_9

--- Seminar_9/routes/employeeRouter.js
+++ Seminar_9/routes/employeeRouter.js
@@ -6,17 +6,37 @@ const router = require("express").Router();
 router
   .route("/employees")
   .get(async (req, res) => {
-    // get all
+    const { name, minSalary, simplified, sortBy } = req.query;
+
     try {
-      const employees = await Employee.findAll({});
+      let whereClause = {};
+      if (minSalary) {
+        whereClause.salary = { [Op.gt]: minSalary };
+      }
+      if (name) {
+        whereClause.firstName = { [Op.like]: `%${name}%` };
+      }
+
+      const employees = await Employee.findAll({
+        where: Object.keys(whereClause).length > 0 ? whereClause : undefined,
+
+        attributes: simplified
+          ? { exclude: ["id", "createdAt", "updatedAt"] }
+          : undefined,
+
+        //SORTARE:
+        // Dacă primim sortBy, ordonăm crescător după acel câmp
+        order: sortBy ? [[sortBy, "ASC"]] : undefined,
+      });
+
       return res.status(200).json(employees);
     } catch (err) {
-      return res.status(500).json(err);
+      return res
+        .status(500)
+        .json({ error: "Eroare la filtrare/sortare", details: err });
     }
   })
   .post(async (req, res) => {
-    // create
-    // console.log("req.body :>> ", req.body);
     try {
       const newEmployee = await Employee.create(req.body);
       return res.status(200).json(newEmployee);
@@ -28,17 +48,13 @@ router
 router
   .route("/employees/:id")
   .get(async (req, res) => {
-    // get by id
-    // filtrarea dupa un camp
-    const employee = await Employee.findOne({
-      where: { id: req.params.id },
-    });
+    const employee = await Employee.findByPk(req.params.id);
     if (employee) {
       return res.status(200).json(employee);
     } else {
       return res
         .status(404)
-        .json({ error: `Employee with id ${req.params.id} does not exists` });
+        .json({ error: `Employee with id ${req.params.id} does not exist` });
     }
   })
   .put(async (req, res) => {
@@ -48,7 +64,20 @@ router
     } else {
       return res
         .status(404)
-        .json({ error: `Employee with id ${req.params.id} does not exists` });
+        .json({ error: `Employee with id ${req.params.id} does not exist` });
+    }
+  })
+  .delete(async (req, res) => {
+    try {
+      const employee = await Employee.findByPk(req.params.id);
+      if (employee) {
+        await employee.destroy();
+        return res.status(200).json({ message: "Employee deleted" });
+      } else {
+        return res.status(404).json({ error: "Employee not found" });
+      }
+    } catch (err) {
+      return res.status(500).json(err);
     }
   });
 
